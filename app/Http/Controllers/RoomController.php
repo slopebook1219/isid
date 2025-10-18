6ab02371f404992bd452ce1d7bbaca6a797a97b9
坂本大幹 <saka.ttt.1219@gmail.com>
2025-10-19 01:35:26 +0900

    feat:質問、部屋、チーム名登録機能の作成

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\Room;
+use Illuminate\Support\Facades\Auth;
+
 
 class RoomController extends Controller
 {
@@ -10,4 +12,48 @@ class RoomController extends Controller
     {
         return view('rooms.index');
     }
+    public function create()
+    {
+        return view('rooms.create');
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'total_teams' => 'required|integer',
+        ]);
+
+        $room = Room::create([
+        'user_id' => Auth::id(),
+        'name' => $request->name,
+        'total_teams' => $request->total_teams,
+    ]);
+
+    return redirect()->route('rooms.setup', $room)
+                 ->with('success', 'ルームを作成しました。');
+    }
+    public function setup(Room $room)
+    {
+        $teams = $room->teams ?? collect();
+
+        return view('rooms.setup', compact('room', 'teams'));
+    }
+
+    public function storeTeams(Request $request)
+    {
+        $room = Room::findOrFail($request->route('room'));
+
+        $request->validate([
+            'teams' => 'required|array',
+            'teams.*' => 'required|string|max:255',
+        ]);
+
+        foreach ($request->teams as $teamName) {
+            $room->teams()->create([
+                'name' => $teamName,
+            ]);
+        }
+        return redirect()->route('rooms.index')->with('success', 'チームを作成しました。');
+    }
+
 }
