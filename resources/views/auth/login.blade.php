97a3b30f25880df54b2858b7a01e736d5a3bc66c
Rikuto Hioki <hioki.rikuto.lab@gmail.com>
2025-11-09 12:03:44 +0900

    correct:レスポンシブ対応ver1

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,43 +2,43 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
+    <form method="POST" action="{{ route('login') }}" class="space-y-6">
         @csrf
 
         <!-- Title -->
-        <h1 class="text-3xl font-bold !text-black dark:!text-white mb-4 text-center">ログイン</h1>
+        <h1 class="text-2xl sm:text-3xl font-bold text-black text-center">
+            ログイン
+        </h1>
 
         <!-- Email Address -->
-        <div>
+        <div class="space-y-2">
             <x-input-label for="email" value="メールアドレス" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <x-text-input id="email" class="block w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-1" />
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
+        <div class="space-y-2">
             <x-input-label for="password" value="パスワード" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
+            <x-text-input id="password" class="block w-full"
                             type="password"
                             name="password"
                             required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            <x-input-error :messages="$errors->get('password')" class="mt-1" />
         </div>
 
         <!-- Remember Me -->
-        <div class="block mt-4">
+        <div>
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">
+                <span class="ms-2 text-sm text-gray-600 dark:text-gray-600">
                     ログイン情報を保存する
                 </span>
             </label>
         </div>
 
         <!-- Login Button -->
-        <div class="mt-4">
+        <div>
             <button type="submit" class="w-full justify-center py-3 bg-lime-500 hover:bg-lime-600 text-white
                  inline-flex items-center px-4 font-semibold rounded-md shadow-sm
                  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-lime-500">
@@ -47,27 +47,27 @@
         </div>
 
         <!-- Forgot Password -->
-        @if (Route::has('password.request'))
-            <div class="mt-4 text-center">
-                <a class="text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-200 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    パスワードを忘れた方はこちら
-                </a>
-            </div>
-        @endif
+        <div class="text-center space-y-6">
+            @if (Route::has('password.request'))
+                <div>
+                    <a class="text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                        パスワードを忘れた方はこちら
+                    </a>
+                </div>
+            @endif
 
-        <!-- Separator line -->
-        <div class="my-6">
-            <hr class="border-gray-300 dark:border-gray-550">
+            <!-- Separator line -->
+            <hr class="border-gray-300 dark:border-gray-700">
+
+            <!-- Register link -->
+            @if (Route::has('register'))
+                <div>
+                    <a href="{{ route('register') }}"
+                    class="text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-500">
+                        新規登録はこちら
+                    </a>
+                </div>
+            @endif
         </div>
-
-        <!-- Register link -->
-        @if (Route::has('register'))
-            <div class="text-center mb-6">
-                <a href="{{ route('register') }}"
-                class="text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-200">
-                    新規登録はこちら
-                </a>
-            </div>
-        @endif
     </form>
 </x-guest-layout>
